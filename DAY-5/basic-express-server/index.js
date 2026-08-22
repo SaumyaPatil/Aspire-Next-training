0ed538a3b8efb740344de133790ad6f265e1493d
thenitinsingh <thenitinsingh2003@gmail.com>
2026-08-22 17:35:27 +0530

    changes in DAY-5

--- DAY-5/basic-express-server/index.js
+++ DAY-5/basic-express-server/index.js
@@ -1,14 +1,8 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-// REST APIs - routing
-app.get("/", (req, res) => {
-  return res.send("Hello From Home Page");
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
 });
 
-app.get("/about", (req, res) => {
-  return res.send(`Hello ${req.query.name}`);
-});
-
-app.listen(8000, () => console.log("Server Started!"));
+app.listen(3000, () => console.log('Server running on port 3000'));
